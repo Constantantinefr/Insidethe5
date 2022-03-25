f2253a0031a01fe24bda29c830488e7d619fdeb3
Constantine <684107@sylvaniastudents.org>
2022-03-25 13:58:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = new express()
-const db = require('better-sqlite3')('InsidetheFive.db')
-
+//const db = require('better-sqlite3')('InsidetheFive.db')
+const port = process.env.port || 8080
+const path = require('path')
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "/client/build")))
+
+app.get("/hello", (req,res) => {
+    res.send("hello world")
+})
 
 app.get('/users', (req, res) => {
     const query = db.prepare("SELECT * FROM users")
