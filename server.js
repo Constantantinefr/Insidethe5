2064fc018d43f37b0a0c04c6ed47740f989833f8
OliverQian7 <71441588+OliverQian7@users.noreply.github.com>
2022-04-07 13:48:22 -0400

    azure stuff

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = new express()
-const db = require('better-sqlite3')('InsidetheFive.db')
+const {CosmosClient} = require ('@azure/cosmos')
+const dbURL = "https://insidethefive2-db.documents.azure.com:443/"
+const dbKey = "zpf8DkuA7D8rV77OqIIugWnbcXix7fVjRezTNLcAR9fEBN4s1hfCw0p9ArF66Bdl0v3r17grrc1xvivsaX1Blw=="
+//const db = require('better-sqlite3')('InsidetheFive.db')
+const conString = "AccountEndpoint=https://insidethefive2-db.documents.azure.com:443/;AccountKey=zpf8DkuA7D8rV77OqIIugWnbcXix7fVjRezTNLcAR9fEBN4s1hfCw0p9ArF66Bdl0v3r17grrc1xvivsaX1Blw==;"
+const port = process.env.PORT || 8080
+const path = require('path')
+const client = new CosmosClient(conString)
+const db = client.database("insidethefive2-db")
+const container = db.container("test")
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "/client/build")))
 
 app.get('/users', (req, res) => {
     const query = db.prepare("SELECT * FROM users")
@@ -12,6 +22,19 @@ app.get('/users', (req, res) => {
     })
 })
 
-app.listen(8080, () => {
+app.get("/hello/:name", async (req,res) => {
+    const data = {
+        id: Date.now().toLocaleString(),
+        score: Math.random() * 100,
+        name: req.params.name
+    }
+    container.items.create(data)
+        .catch((err) => {console.error(err)})
+    const hellos = await container.items.query("SELECT * FROM c")
+        .fetchAll()
+    res.send(JSON.stringify(hellos))
+})
+
+app.listen(port, () => {
     console.log("hi")
 })
