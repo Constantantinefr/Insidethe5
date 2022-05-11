e8920446c3966a3b00ea5b8387c4604f586f1343
Constantine <684107@sylvaniastudents.org>
2022-05-11 14:04:52 -0400

    Update Order.js

--- client/src/Components/Tabs/Order.js
+++ client/src/Components/Tabs/Order.js
@@ -5,7 +5,145 @@ import { Button } from 'reactstrap';
 function Order() {
     return (
         <div>
-            <Button color = "orange">Order Now!</Button>
+           
+            <form action="" method="post">
+        <label for="Cards">Loaded Beer Queso: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Cauliflower Bites: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Bavarian Pretzel: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Street Corn Dip: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Jalapeno Bacon Balls: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Pub Burger: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">The IT5 burger: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">The Medi Wrap: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">The Nashviller: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Mac N Cheese Bowl: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Beef stroganoff: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">ITF Meatloaf: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">Chicken + Waffles: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="Cards">The Cubano: </label>
+        <select name="Cards1" id="Cards">
+        <option value="">Select</option>
+        <option id="1" value="Knight">0</option>
+        <option id="2"value="Mini Pekka">1</option>
+        <option id="3"value="Royal Ghost">2</option>
+        <option id="4"value="Elixer Golem">3</option>
+        </select>
+        <br></br>
+        <label for="first-name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">First name*</label>
+        <input name="first-name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+        <label for="last-name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Last name*</label>
+        <input name="last-name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+        <label for="Address-name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Address*</label>
+        <input name="Address-name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+        <label for="Phone-name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Phone Number</label>
+        <input name="Phone-name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+        <br></br>
+        </form>
+        <Button color = "danger">Order Now!</Button>
         </div>
     )
 }
