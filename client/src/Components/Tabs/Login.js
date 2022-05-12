6a14f2b442f47ca753ac38328e9ca7e40fdc45f9
OliverQian7 <71441588+OliverQian7@users.noreply.github.com>
2022-05-12 13:29:41 -0400

    broken

--- client/src/Components/Tabs/Login.js
+++ client/src/Components/Tabs/Login.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
 import Register from './Register'
 
-const username
-const name 
-const password 
-const address 
+function Login() {
+
+  const [username,setUsername] = useState("const")
+const [fullname, setFullname] = useState("constantine rusous")
+const [password,setPassword] = useState("constantine password")
+const [address, setAddress] = useState("123 street lane") 
 
 Register = (e) => {
   const config = {
@@ -14,15 +16,13 @@ Register = (e) => {
     },
     "body" : JSON.stringify ({
       username,
-      name,
+      fullname,
       password,
       address
     })
   }
   fetch('/register', config)
 }
-
-function Login() {
     return (
         <div class="bg-white py-6 sm:py-8 lg:py-12">
         <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
@@ -32,7 +32,7 @@ function Login() {
             <div class="flex flex-col gap-4 p-4 md:p-8">
               <div>
                 <label for="username" class="inline-block text-gray-800 text-sm sm:text-base mb-2">Username</label>
-                <input name="username" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                <input onChange    name="username" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
               </div>
 
               <div>
